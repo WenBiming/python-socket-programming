552a12c5f7653d09f75864742ce869c3b32e2bf4
WenBiming <m18077021652@163.com>
2021-12-07 20:26:28 +0800

    lab1 arbitary server

--- udpserver.py
+++ udpserver.py
@@ -1,23 +1,42 @@
 import socket
+import sys
 
 
-ip = '127.0.0.1'
-port = 20001
-buffersize = 1024
+def serverOf(ip):
 
-msgFromServer = 'This is upd server speaking'
-bytesToSend = str.encode(msgFromServer)
-s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-s.bind((ip, port))
-print('UDP server up and listening')
-count = 0
-while True:
-    message, address = s.recvfrom(buffersize)
-    print('Message from Client:{}'.format(message))
-    print('client IP address: {}'.format(address))
-    s.sendto(bytesToSend, address)
-    count += 1
-    if count == 10:
-        break
+    port = 20001
+    buffersize = 1024
 
-s.close()
+
+    msgFromServer = 'This is upd server speaking'
+    bytesToSend = str.encode(msgFromServer)
+    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    hostname = socket.gethostname()
+    hostip = socket.gethostbyname(hostname)
+
+
+    s.bind((ip, port))
+
+
+    print('UDP server {}\t{} up and listening'.format(hostname, hostip))
+    count = 0
+    while True:
+        message, address = s.recvfrom(buffersize)
+        print('Message from Client:{}'.format(message))
+        print('client IP address: {}'.format(address))
+        
+        s.sendto(bytesToSend, address)
+        count += 1
+        if count == 10:
+            break
+
+    s.close()
+
+if __name__ == '__main__':
+    try:
+        client = sys.argv[1]
+    except IndexError:
+        client = ''
+    finally:
+        serverOf(client)
