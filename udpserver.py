9ab24884f30cded49f20b034771d4dc98f939415
WenBiming <m18077021652@163.com>
2021-12-07 09:01:00 +0800

    ping using icmp protocol

--- udpserver.py
+++ udpserver.py
@@ -9,9 +9,14 @@ bytesToSend = str.encode(msgFromServer)
 s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 s.bind((ip, port))
 print('UDP server up and listening')
-
+count = 0
 while True:
     message, address = s.recvfrom(buffersize)
     print('Message from Client:{}'.format(message))
     print('client IP address: {}'.format(address))
     s.sendto(bytesToSend, address)
+    count += 1
+    if count == 10:
+        break
+
+s.close()
